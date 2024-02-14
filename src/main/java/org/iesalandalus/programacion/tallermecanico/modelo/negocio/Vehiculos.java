18bcde44d3e4f3fbe678b114a5ad5afd2e2726a0
Rythor15 <03rubenbenben15@gmail.com>
2024-02-14 12:06:22 +0100

    clase Vehiculos
    completada y pasados los test

---
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/negocio/Vehiculos.java
@@ -0,0 +1,44 @@
+package org.iesalandalus.programacion.tallermecanico.modelo.negocio;
+
+import org.iesalandalus.programacion.tallermecanico.modelo.dominio.Cliente;
+import org.iesalandalus.programacion.tallermecanico.modelo.dominio.Vehiculo;
+
+import javax.naming.OperationNotSupportedException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Vehiculos {
+    private final List<Vehiculo> coleccionVehiculos;
+
+    public Vehiculos() {
+        coleccionVehiculos = new ArrayList<>();
+    }
+
+    public List<Vehiculo> get() {
+        return new ArrayList<>(coleccionVehiculos);
+    }
+
+    public void insertar(Vehiculo vehiculo) throws OperationNotSupportedException {
+        Objects.requireNonNull(vehiculo, "No se puede insertar un vehículo nulo.");
+        if (coleccionVehiculos.contains(vehiculo)) {
+            throw new OperationNotSupportedException("Ya existe un vehículo con esa matrícula.");
+        }
+        coleccionVehiculos.add(vehiculo);
+    }
+
+    public Vehiculo buscar(Vehiculo vehiculo) {
+        Objects.requireNonNull(vehiculo, "No se puede buscar un vehículo nulo.");
+        int indiceListaClientes = coleccionVehiculos.indexOf(vehiculo);
+        return (coleccionVehiculos.contains(vehiculo)) ? coleccionVehiculos.get(indiceListaClientes) : null;
+    }
+
+    public void borrar(Vehiculo vehiculo) throws OperationNotSupportedException {
+        Objects.requireNonNull(vehiculo, "No se puede borrar un vehículo nulo.");
+        if (coleccionVehiculos.contains(vehiculo)) {
+            coleccionVehiculos.remove(vehiculo);
+        } else {
+            throw new OperationNotSupportedException("No existe ningún vehículo con esa matrícula.");
+        }
+    }
+}
